8e68ece44b99cdbe4e62234e16feb4857c0a1bfc
Kim Carey <kthomas06@gmail.com>
2020-02-15 16:40:47 -0800

    Set up start of the game.

---
+++ script.py
@@ -0,0 +1,28 @@
+import time
+
+questions = {
+    '1': 'Question 1?',
+    '2': 'Question 2?',
+    '3': 'Question 3?',
+    '4': 'Question 4?',
+    '5': 'Question 5?',
+    '6': 'Question 6?',
+    '7': 'Question 7?',
+    '8': 'Question 8?',
+    '9': 'Question 9?',
+    '10': 'Question 10?',
+    '11': 'Question 11?',
+    '12': 'Question 12?',
+    '13': 'Question 13?',
+    '14': 'Question 14?',
+    '15': 'Question 15?'
+}
+
+print('So you think you\'re a New Yorker?')
+time.sleep(2)
+print('Take this quiz to see if you\'re a true New Yorker at heart.')
+time.sleep(2)
+print('Are you ready?')
+time.sleep(2)
+print('Begin...')
+
