06a9e1d703f04b3a7bd3faeba967c07f66df89f9
Kim Carey <kthomas06@gmail.com>
2020-02-18 10:36:41 -0800

    Fixed for loop which limits quiz to 8 questions.

--- test.py
+++ test.py
@@ -15,12 +15,12 @@ def index():
 
 
 question_prompts = [
-        'Which has never been a NYC subway line?\n (a) J\n (b) K\n (c) 9\n (d) Q\n\n',
+        'Which has never been a NYC subway line?\n (a) J\n (b) 9\n (c) K\n (d) Q\n\n',
         'Which borough is used in the moniker for the New York Yankees?\n (a) Queens\n (b) Bronx\n (c) Brooklyn\n (d) Manhattan\n\n',
         'What famous hotel has a secret train platform beneath it?\n (a) Waldorf Astoria\n (b) The Plaza\n (c) The Carlyle\n (d) Gramercy Park Hotel\n\n',
         'How many steps lead to the crown of the Statue of Liberty?\n (a) What\'s the Statue of Liberty?\n (b) 327\n (c) 973\n (d) Idk, no one goes there besides tourists.\n\n',
         'What famous NYC landmark was originally used as a burial ground?\n (a) Central Perk\n (b) Washington Square Park\n (c) Central Park\n (d) Coney Island\n\n',
-        'Which island was used a hospital for criminals, sick, and mentally ill patients:\n (a) Rikers Island\n (b) Long Island\n (c) Staten Island\n (d) Roosevelt Island\n\n',
+        'Which island was used a hospital for criminals, sick, and mentally ill patients:\n (a) Rikers Island\n (b) Long Island\n (c) Roosevelt Island\n (d) Staten Island\n\n',
         'Which subway line can tout being the longest?\n (a) 7\n (b) A\n (c) S\n (d) D\n\n',
         'Which acronym is relevant to New York?\n (a) LGA\n (b) BQE\n (c) NDQ\n (d) All of the above\n\n',
         'What was Citifield\'s original name before it was changed in 2006?\n (a) Shea Stadium\n (b) Mets Piazza\n (c) Seaver Stadium\n (d) Flushing Meadows Park\n\n',
@@ -34,15 +34,21 @@ question_prompts = [
 
 
 questions = [
-    Questions(question_prompts[0], 'b'),
+    Questions(question_prompts[0], 'c'),
     Questions(question_prompts[1], 'b'),
     Questions(question_prompts[2], 'a'),
     Questions(question_prompts[3], 'd'),
     Questions(question_prompts[4], 'b'),
-    Questions(question_prompts[5], 'd'),
+    Questions(question_prompts[5], 'c'),
     Questions(question_prompts[6], 'b'),
-    Questions(question_prompts[7], 'd')
-
+    Questions(question_prompts[7], 'd'),
+    Questions(question_prompts[8], 'a'),
+    Questions(question_prompts[9], 'a'),
+    Questions(question_prompts[10], 'b'),
+    Questions(question_prompts[11], 'c'),
+    Questions(question_prompts[12], 'a'),
+    Questions(question_prompts[13], 'd'),
+    Questions(question_prompts[14], 'b')
 ]
 
 # Randomize the questions
@@ -55,23 +61,27 @@ def run_quiz(q_list):
     counter = 0
 
     # Limit to a total of 8 questions
-    if counter < 7:
-        for question in q_list:
-            answer = input(question.prompt)
-            if answer == question.answer:
-                score += 3
-            counter += 1
+    for question in q_list:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 3
+        counter += 1
 
-        print('You got ' + str(score) + '/' + str(len(q_list) * 3) + ' points.')
+        if counter == 8:
+            break
 
-        if score >= 0 and score <= 6:
-            print('Fuggedaboutit! You are no New Yorker!')
-        if score >= 7 and score <= 12:
-            print('Not quite... but you\'re still better than Staten Island!')
-        if score >= 13 and score <= 19:
-            print('You\'re getting warmer than a subway car during the morning commute!')
-        if score >= 20 and score <= 24:
-            print('Start spreading the news! You\'re an official New Yorker!')
+    # print('You got ' + str(score) + '/' + str(len(q_list) * 3) + ' points.')
+    print('You got ' + str(score) + '/' + ' 24 points.')
+
+
+    if score >= 0 and score <= 6:
+        print('Fuggedaboutit! You are no New Yorker!')
+    if score >= 7 and score <= 12:
+        print('Not quite... but you\'re still better than Staten Island!')
+    if score >= 13 and score <= 19:
+        print('You\'re getting warmer than a subway car during the morning commute!')
+    if score >= 20 and score <= 24:
+        print('Start spreading the news! You\'re an official New Yorker!')
 
 
 run_quiz(questions_random)
